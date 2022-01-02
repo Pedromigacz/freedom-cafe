edb30ef17aef81f3cf05e43ffb438ae3436c8f5e
Pedro Henrique Guimaraes Migacz <pedromigacz@gmail.com>
2022-01-02 20:32:39 -0300

    add defaultLayout

--- src/templates/DefaultLayout/index.jsx
+++ src/templates/DefaultLayout/index.jsx
@@ -1,11 +1,16 @@
 import * as React from "react"
 import "./reset.css"
+import styled from "styled-components"
 
 const DefaultLayout = ({ children }) => (
-  <>
-    <h1>AAAAAAAAAAAAAAA</h1>
-    {children}
-  </>
+  <ContentContainer>{children}</ContentContainer>
 )
+
+const ContentContainer = styled.div`
+  display: inline-block;
+  width: 100%;
+  min-height: 100vh;
+  background: linear-gradient(243.18deg, #4d194d 0%, #006466 100%);
+`
 
 export default DefaultLayout
